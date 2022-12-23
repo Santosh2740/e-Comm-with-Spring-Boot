38e597c683edc365b411e008fc2db67125a8be6f
Santosh2704 <santoshvishwa1572@gmail.com>
2022-12-23 10:36:29 +0530

    API

---
+++ backend/Spring-Boot-Ecommerce/Spring-Boot-Ecommerce/src/main/java/com/NiruSoft/Ecommerce/entity/ProductCategory.java
@@ -0,0 +1,21 @@
+package com.NiruSoft.Ecommerce.entity;
+ import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+// @Entity
+ @Table(name="product_category")
+ @Getter
+ @Setter
+public class ProductCategory {
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+     @Column(name = "id")
+     private Long id;
+
+     @Column(name="category_name")
+     private String categoryName;
+
+     @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+     private set<Product> products;
+}
